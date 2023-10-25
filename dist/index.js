94d346c6f5928594df124133195bf0d6a951fc6d
ikkitang <new_ittyandesu@outlook.jp>
2023-10-26 00:12:28 +0900

    feat: action.ymlを調整して、main側もそれに依存するようにした
    feat: bundle 結果を取り込む
    
    test: テスト結果がコケてたので治す

--- dist/index.js
+++ dist/index.js
@@ -2725,8 +2725,7 @@ exports["default"] = _default;
 /***/ 713:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
-const core = __nccwpck_require__(186)
-const { wait } = __nccwpck_require__(312)
+const core = __nccwpck_require__(186);
 
 /**
  * The main function for the action.
@@ -2734,51 +2733,27 @@ const { wait } = __nccwpck_require__(312)
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const issueNumber = core.getInput('issue_number', { required: true });
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('time', [
+      {
+        owner,
+        repo,
+        issueNumber
+      }
+    ]);
   } catch (error) {
     // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
 module.exports = {
   run
-}
-
-
-/***/ }),
-
-/***/ 312:
-/***/ ((module) => {
-
-/**
- * Wait for a number of milliseconds.
- *
- * @param {number} milliseconds The number of milliseconds to wait.
- * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-  return new Promise(resolve => {
-    if (isNaN(milliseconds)) {
-      throw new Error('milliseconds not a number')
-    }
-
-    setTimeout(() => resolve('done!'), milliseconds)
-  })
-}
-
-module.exports = { wait }
+};
 
 
 /***/ }),
@@ -2915,9 +2890,9 @@ var __webpack_exports__ = {};
 /**
  * The entrypoint for the action.
  */
-const { run } = __nccwpck_require__(713)
+const { run } = __nccwpck_require__(713);
 
-run()
+run();
 
 })();
 
