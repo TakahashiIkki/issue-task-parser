8a292982f2ef37f08c471d2100936f2484b21d55
ikkitang <new_ittyandesu@outlook.jp>
2023-10-26 00:12:28 +0900

    feat: action.ymlを調整して、main側もそれに依存するようにした
    feat: bundle 結果を取り込む
    
    test: テスト結果がコケてたので治す

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -5,96 +5,125 @@
  * Specifically, the inputs listed in `action.yml` should be set as environment
  * variables following the pattern `INPUT_<INPUT_NAME>`.
  */
-const core = require('@actions/core')
-const main = require('../src/main')
+const core = require('@actions/core');
+const main = require('../src/main');
 
 // Mock the GitHub Actions core library
-const debugMock = jest.spyOn(core, 'debug').mockImplementation()
-const getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
-const setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
-const setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+const debugMock = jest.spyOn(core, 'debug').mockImplementation();
+const getInputMock = jest.spyOn(core, 'getInput').mockImplementation();
+const setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation();
+const setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation();
 
 // Mock the action's main function
-const runMock = jest.spyOn(main, 'run')
+const runMock = jest.spyOn(main, 'run');
 
 // Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
+const timeRegex = /^\d{2}:\d{2}:\d{2}/;
 
 describe('action', () => {
   beforeEach(() => {
-    jest.clearAllMocks()
-  })
+    jest.clearAllMocks();
+  });
 
   it('sets the time output', async () => {
     // Set the action's inputs as return values from core.getInput()
     getInputMock.mockImplementation(name => {
       switch (name) {
-        case 'milliseconds':
-          return '500'
+        case 'owner':
+          return 'TakahashiIkki';
+        case 'repo':
+          return 'issue-task-parser';
+        case 'issue_number':
+          return 50;
         default:
-          return ''
+          return '';
       }
-    })
+    });
 
-    await main.run()
-    expect(runMock).toHaveReturned()
+    await main.run();
+    expect(runMock).toHaveReturned();
 
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
-  })
-
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation(name => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
-        default:
-          return ''
+    expect(setOutputMock).toHaveBeenNthCalledWith(1, 'time', [
+      {
+        owner: 'TakahashiIkki',
+        repo: 'issue-task-parser',
+        issueNumber: 50
       }
-    })
+    ]);
+  });
 
-    await main.run()
-    expect(runMock).toHaveReturned()
+  describe('failed when required parameter', () => {
+    it('fails when owner is not provided', async () => {
+      // Set the action's inputs as return values from core.getInput()
+      getInputMock.mockImplementation(name => {
+        switch (name) {
+          case 'owner':
+            throw new Error('Input required and not supplied: owner');
+          case 'repo':
+            return 'issue-task-parser';
+          case 'issue_number':
+            return 50;
+          default:
+            return '';
+        }
+      });
 
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
-    )
-  })
+      await main.run();
+      expect(runMock).toHaveReturned();
 
-  it('fails if no input is provided', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation(name => {
-      switch (name) {
-        case 'milliseconds':
-          throw new Error('Input required and not supplied: milliseconds')
-        default:
-          return ''
-      }
-    })
+      // Verify that all of the core library functions were called correctly
+      expect(setFailedMock).toHaveBeenNthCalledWith(
+        1,
+        'Input required and not supplied: owner'
+      );
+    });
+    it('fails when repo is not provided', async () => {
+      // Set the action's inputs as return values from core.getInput()
+      getInputMock.mockImplementation(name => {
+        switch (name) {
+          case 'owner':
+            return 'TakahashiIkki';
+          case 'repo':
+            throw new Error('Input required and not supplied: repo');
+          case 'issue_number':
+            return 50;
+          default:
+            return '';
+        }
+      });
 
-    await main.run()
-    expect(runMock).toHaveReturned()
+      await main.run();
+      expect(runMock).toHaveReturned();
 
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'Input required and not supplied: milliseconds'
-    )
-  })
-})
+      // Verify that all of the core library functions were called correctly
+      expect(setFailedMock).toHaveBeenNthCalledWith(
+        1,
+        'Input required and not supplied: repo'
+      );
+    });
+    it('fails when issue_number is not provided', async () => {
+      // Set the action's inputs as return values from core.getInput()
+      getInputMock.mockImplementation(name => {
+        switch (name) {
+          case 'owner':
+            return 'TakahashiIkki';
+          case 'repo':
+            return 'issue-task-parser';
+          case 'issue_number':
+            throw new Error('Input required and not supplied: issue_number');
+          default:
+            return '';
+        }
+      });
+
+      await main.run();
+      expect(runMock).toHaveReturned();
+
+      // Verify that all of the core library functions were called correctly
+      expect(setFailedMock).toHaveBeenNthCalledWith(
+        1,
+        'Input required and not supplied: issue_number'
+      );
+    });
+  });
+});
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-const core = require('@actions/core')
-const { wait } = require('./wait')
+const core = require('@actions/core');
 
 /**
  * The main function for the action.
@@ -7,24 +6,24 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const issueNumber = core.getInput('issue_number', { required: true });
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('time', [
+      {
+        owner,
+        repo,
+        issueNumber
+      }
+    ]);
   } catch (error) {
     // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
 module.exports = {
   run
-}
+};
